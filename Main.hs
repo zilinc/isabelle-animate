35462173f63b151bd405df45f12044b970725166
Zilin Chen <zilin.chen@ntu.edu.sg>
2025-05-12 01:12:53 +0800

    port from the coq experiment

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Main where
 
 import Control.Applicative (Alternative(..))
@@ -12,19 +14,42 @@ import PredList
 
 main :: IO ()
 main = do
-  let Seq a1 = ex1
-      Seq a2 = ex2
-      Seq a3 = ex3
-      Seq a3' = ex3'
-  putStrLn $ show a1
-  putStrLn $ show a2
-  putStrLn $ show a3
-  putStrLn $ show a3'
+  dispExs
+    [ Ex $ append_1_2 [1::Int,2,3] [4,5]
+    , Ex $ append_3 [1::Int,2,3,4,5]
+    , Ex $ append_1_3 [1::Int,2] [1,2,3,4,5]
+    , Ex $ append_1_3 [1::Int,3] [1,2,3,4,5]
+    ]
 
-ex1 = append_1_2 [1::Int,2,3] [4,5]
-ex2 = append_3 [1::Int,2,3,4,5]
-ex3 = append_1_3 [1::Int,2] [1,2,3,4,5]
-ex3' = append_1_3 [1::Int,3] [1,2,3,4,5] 
+  dispExs
+    [ Ex $ baz_1_2 3 (MyCr1 "my" 3)
+    , Ex $ baz_1_2 3 (MyCr1 "my" 5)
+    , Ex $ baz_1_2 3 (MyCr2 3)
+    ]
+
+  dispExs [ Ex $ baz_1_3 3 "yours" ]
+
+
+
+
+-- /////////////////////////////////////////////////////////////////////////////
+-- Utils
+
+data Ex = forall t. (Show t) => Ex (Pred t)
+
+dispEx :: (Show t) => Pred t -> IO ()
+dispEx (Seq x) = putStrLn $ "▹ " ++ show x
+
+dispExs :: [Ex] -> IO ()
+dispExs [] = divLine
+dispExs (Ex ex:exs) = dispEx ex >> dispExs exs
+
+divLine :: IO ()
+divLine = putStrLn $ replicate 80 '-'
+
+
+-- /////////////////////////////////////////////////////////////////////////////
+-- `append` example from the Isabelle animation paper
 
 {- [Isabelle]
 
@@ -60,3 +85,87 @@ append_1_3 xs zs =
     ([], _) -> empty
     (w:ws, v:vs) | w == v -> append_1_3 ws vs
     _ -> empty)
+
+
+-- /////////////////////////////////////////////////////////////////////////////
+-- `myType` example from Avishkar's Rocq experiments
+
+
+{- [Rocq]
+
+Inductive myType : Set :=
+| mycr2 : nat -> myType
+| mycr4 : string -> nat -> myType
+| mycr1 : string -> nat -> myType
+| mycr3 : myType.
+
+Inductive baz : nat -> myType -> string -> Prop :=
+| bazCon : forall (a : nat), forall (x : myType), forall (b : string),
+    mycr1 b a = x -> baz a x b.
+
+-}
+
+type Nat = Int
+
+data MyType where
+  MyCr2 :: Nat -> MyType
+  MyCr4 :: String -> Nat -> MyType
+  MyCr1 :: String -> Nat -> MyType
+  MyCr3 :: MyType
+  deriving (Show)
+
+-- XXX | {- [Rewrite = on `myType` type to an inductive definition]
+-- XXX | 
+-- XXX | Inductive myTypeEq : myType -> myType -> Prop :=
+-- XXX | | mycr2_eq : forall (n m : nat), n = m -> myTypeEq (mycr2 n) (mycr2 m)
+-- XXX | | mycr4_eq : forall (u v : string), forall (m n : nat),
+-- XXX |     u = v -> m = n -> myTypeEq (mycr4 u m) (mycr4 v n)
+-- XXX | | mycr1_eq : forall (u v : string), forall (m n : nat),
+-- XXX |     u = v -> m = n -> myTypeEq (mycr1 u m) (mycr1 v n)
+-- XXX | | mycr3_eq : myTypeEq mycr3 mycr3.
+-- XXX | 
+-- XXX | -}
+
+
+mycr1_eq_2_3 :: Nat -> MyType -> Pred String
+mycr1_eq_2_3 a x =
+  pure (a, x) >>= \case
+    (a', MyCr1 c y) | a' == y -> pure c
+    _ -> empty
+
+baz_1_2 :: Nat -> MyType -> Pred String
+baz_1_2 a x =
+  pure (a, x) >>= \case
+    (a', x') -> mycr1_eq_2_3 a' x' >>= \b -> pure b
+
+
+mycr1_eq_1_2 :: Nat -> String -> Pred MyType
+mycr1_eq_1_2 a b =
+  pure (a, b) >>= \case
+    (a', b') -> pure (MyCr1 b' a')
+
+baz_1_3 :: Nat -> String -> Pred MyType
+baz_1_3 a b =
+  pure (a, b) >>= \case
+    (a', b') -> mycr1_eq_1_2 a' b'
+
+-- What is the procedure to animate the equality?
+
+
+-- /////////////////////////////////////////////////////////////////////////////
+-- Similar to Avishkar's example above, but with a false premise.
+{- [Rocq]
+
+Inductive myType : Set :=
+| mycr2 : nat -> myType
+| mycr4 : string -> nat -> myType
+| mycr1 : string -> nat -> myType
+| mycr3 : myType.
+
+Inductive foo : nat -> myType -> string -> Prop :=
+| fooCon : forall (a b : nat), forall (x : myType), forall (s t : string),
+    mycr1 s a = mycr4 t b -> foo s a t b.
+
+-}
+
+-- How to animate the false premise?
